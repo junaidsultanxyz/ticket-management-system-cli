fa67c5dc443208175a08462b87ecc7fe2aeb8cc1
junaid sultan <jsultanwork@gmail.com>
2026-01-16 18:30:00 +0500

    entities added

--- src/main/java/com/junaidsultan/Application.java
+++ src/main/java/com/junaidsultan/Application.java
@@ -2,7 +2,7 @@ package com.junaidsultan;
 
 public class Application {
 
-    static void main(String[] args) {
+    static void main() {
 
     }
 }
---
+++ src/main/java/com/junaidsultan/ui/ConsoleWindow.java
@@ -0,0 +1,11 @@
+package com.junaidsultan.ui;
+
+public class ConsoleWindow {
+    public static void Start() {
+        IO.println();
+    }
+
+    private static void printHeader (String heading) {
+        IO.println();
+    }
+}
