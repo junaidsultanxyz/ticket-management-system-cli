9f320b589733e99bc901e8a6f7ed5a9892dc18a4
junaid sultan <jsultanwork@gmail.com>
2026-01-16 17:48:39 +0500

    main cleaned

--- src/main/java/com/junaidsultan/Application.java
+++ src/main/java/com/junaidsultan/Application.java
@@ -1,11 +1,5 @@
 package com.junaidsultan;
 
-import com.junaidsultan.config.DBConnection;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Application {
 
     static void main(String[] args) {
